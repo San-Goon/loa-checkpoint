6effaf656b9159db80adf678d5d49f8177418bf4
조강훈 <ganghun.jo@lgepartner.com>
2024-02-22 10:26:58 +0900

    feat: implement OCR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,75 @@
-"use client"
+"use client";
 
-import {useCallback, useEffect, useRef, useState} from "react";
-import {Button} from "@/components/ui/button";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { createWorker } from "tesseract.js";
 
 export default function Home() {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const [stream, setStream] = useState<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [stream, setStream] = useState<MediaStream | null>(null);
+
+  const captureVideo = useCallback(() => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    if (!video || !canvas) return;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    initializeTesseract(canvas);
+  }, [videoRef, canvasRef]);
 
   const startVideo = useCallback(async () => {
-    const mediaStream =  await navigator.mediaDevices.getDisplayMedia({video: true})
-      setStream(mediaStream);
-  }, [])
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
+    setStream(mediaStream);
+  }, []);
 
-    const stopVideo = useCallback(() => {
-        if (stream) {
-            const tracks = stream.getTracks();
-            tracks.forEach(track => track.stop());
-            setStream(null)
-        }
-    }, [stream])
+  const stopVideo = useCallback(() => {
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      setStream(null);
+    }
+  }, [stream]);
 
-    useEffect(() => {
-        if(stream && videoRef.current) {
-            videoRef.current.srcObject = stream;
-        }
-    }, [stream])
+  useEffect(() => {
+    if (stream && videoRef.current) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  interface Lang {
+    code: string;
+    data: unknown;
+  }
+
+  const initializeTesseract = async (canvas: HTMLCanvasElement) => {
+    // @ts-ignore
+    const worker = await createWorker(["kor", "eng"]);
+    const {
+      data: { text },
+    } = await worker.recognize(canvas);
+    console.log(text);
+    await worker.terminate();
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <video ref={videoRef} autoPlay muted />
-          <Button onClick={startVideo}>화면공유</Button>
-          <Button onClick={stopVideo}>공유중단</Button>
+        <video ref={videoRef} autoPlay muted />
+        <canvas className="" ref={canvasRef} />
+        <Button onClick={startVideo}>화면공유</Button>
+        <Button onClick={stopVideo}>공유중단</Button>
+        <Button onClick={captureVideo}>캡처</Button>
       </div>
     </main>
   );
