c30a8c8db1e9138c3f6b65ad4dac51ea6e1ec86b
San-Goon <tigerlove95@naver.com>
2024-02-28 12:27:48 +0900

    fix: fix hydration warning by using shadcn theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
