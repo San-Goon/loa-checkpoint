a093b2ef90ef35de77171994d567d4911a610182
San-Goon <tigerlove95@naver.com>
2024-03-06 15:33:30 +0900

    chore: update UI

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 export default function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="flex h-16 px-6 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
             <span className="inline-block font-bold">로아검문소</span>
