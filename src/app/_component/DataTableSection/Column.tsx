de84c08433e8285a1037ea64231a8b26d8e9ac2f
San-Goon <tigerlove95@naver.com>
2024-03-20 14:08:58 +0900

    feat: show title

--- src/app/_component/DataTableSection/Column.tsx
+++ src/app/_component/DataTableSection/Column.tsx
@@ -14,6 +14,7 @@ import {
 
 export type Info = {
   name: string;
+  title: string;
   mainEngraving: string;
   equipment: {
     level: string;
@@ -50,8 +51,11 @@ export const columns: ColumnDef<Info>[] = [
         return (
           <TooltipProvider>
             <Tooltip>
+              <p>{row.original.title}</p>
               <TooltipTrigger asChild>
-                <p className="font-bold text-red-600">{row.original.name}</p>
+                <p className="font-bold text-red-600 mt-2">
+                  {row.original.name}
+                </p>
               </TooltipTrigger>
               <TooltipContent>
                 <p>차단된 캐릭터명:{row.original.isBanned.name}</p>
@@ -61,7 +65,12 @@ export const columns: ColumnDef<Info>[] = [
           </TooltipProvider>
         );
       }
-      return <div>{row.original.name}</div>;
+      return (
+        <div>
+          <p>{row.original.title}</p>
+          <p>{row.original.name}</p>
+        </div>
+      );
     },
   },
   {
