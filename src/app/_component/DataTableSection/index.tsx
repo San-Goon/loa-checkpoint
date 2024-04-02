3fabba4d31393402d04c4b2ed5aa3b73ce949bde
San-Goon <tigerlove95@naver.com>
2024-04-02 10:45:02 +0900

    feat: implement exceptional process

--- src/app/_component/DataTableSection/index.tsx
+++ src/app/_component/DataTableSection/index.tsx
@@ -8,12 +8,16 @@ import { useQueries } from "@tanstack/react-query";
 import { getCharacterInfo } from "@/app/_lib/getCharacterInfo";
 import { responseProcessor } from "@/lib/utils";
 import { useBlacklistStore } from "@/store/blacklist";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function DataTableSection() {
   const blacklist = useBlacklistStore((state) => state.blacklist);
   const recognized = useCharactersStore((state) => state.recognized);
   const typed = useCharactersStore((state) => state.typed);
+  const deleteName = useCharactersStore((state) => state.deleteName);
   const token = useTokenStore((state) => state.token);
+
+  const { toast } = useToast();
 
   const recognizedQueries = useQueries({
     queries: recognized.map((name) => ({
@@ -35,6 +39,23 @@ export default function DataTableSection() {
 
   const data = [...recognizedQueries, ...typedQueries].map((query) => {
     if (query.isLoading) return "loading";
+    if (query.data.error) {
+      deleteName(query.data.name);
+      if (query.data.error === "Unauthorized") {
+        toast({
+          variant: "destructive",
+          title: "API 키가 잘못되었습니다.",
+          description: "API 키를 확인 후 재시도해주세요.",
+        });
+      }
+      if (query.data.error === "Rate Limit Exceeded") {
+        toast({
+          variant: "destructive",
+          title: "API 요청 횟수가 초과되었습니다.",
+          description: "잠시후에 다시 시도해주세요.",
+        });
+      }
+    }
     return responseProcessor(query.data, blacklist);
   });
 
